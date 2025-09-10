b53494977a1db8f8af5a65d188cf48083f293334
Tanisha Sharma <tanishaofficial30@gmail.com>
2025-09-11 00:15:49 +1000

    dp and tree questions

---
+++ Patterns/DFS/CourseSchedule.py
@@ -0,0 +1,40 @@
+# Leetcode 207 Medium
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # build adjacency list of prerequisites
+        prereq = {c: [] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
+        # basically courses as keys and their prereqs as values
+
+        # a course has 3 possible states
+        # visited -> crs have been added to output
+        # visiting -> crs not added to output, but added to cycle
+        # unvisited -> crs not added to output or cycle
+        output = []
+        visit, cycle = set(), set()
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                # cycle present
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+
+        for c in range(numCourses):
+            if dfs(c) == False:
+                return False
+
+        return True
---
+++ Patterns/Dynamic-Prog/HouseRobber.py
@@ -0,0 +1,14 @@
+# Leetcode 198 Easy
+from typing import List
+
+# decision tree
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1,rob2=0,0
+
+        # [rob1, rob2, n, n+1, ...]
+        for n in nums:
+            temp=max(rob1+n,rob2)
+            rob1=rob2
+            rob2=temp
+        return rob2
