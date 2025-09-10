5db9eba5826bf4f976e2a6203edcdcae3f3ff618
Tanisha Sharma <tanishaofficial30@gmail.com>
2025-09-11 00:21:15 +1000

    dp questions

---
+++ Patterns/Dynamic-Prog/CoinChange.py
@@ -0,0 +1,18 @@
+# Leetcode 322 Medium
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+
+        res = dp[amount]
+        if res != amount + 1:
+            return res
+        return -1
